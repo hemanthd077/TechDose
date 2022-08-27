a1dfe9d017934bf79dc5c0c38fa360b68acfa19e
hemanthd077 <hemanth.d2020cce@sece.ac.in>
2022-08-27 22:15:51 +0530

    Single Number

---
+++ Bit_Manipulation_Day_3/LC_136_SINGLE_NUMBER.c
@@ -0,0 +1,15 @@
+// LC : 136 -> Single Number C programing
+
+
+
+int singleNumber(int* nums, int numsSize){
+   if(numsSize==1){
+       return nums[0];
+   }
+    int res=nums[0];
+    for(int i=1;i<numsSize;i++){
+        res^=nums[i];
+    }
+    return res;
+}
+
